277a89034fa3ac549207b812cfa0b7b1845f6ecc
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-06 15:48:42 -0400

    Initial Commit

--- cogs/afk.py
+++ cogs/afk.py
@@ -11,17 +11,24 @@ class AFK:
     @commands.group(pass_context=True, aliases=['away'])
     async def afk(self, ctx):
         """Set as AFK so users will know to stop bothering you."""
-        ...
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("`Incorrect Usage`\n```siri afk on|enable [reason]``` or ```siri afk off|disable```)
         
     @afk.command(pass_context=True, aliases=['enable'])
-    async def on(self, ctx):
+    async def on(self, ctx, *, reason=None):
         """Turn AFK On (AFK SUB)"""
-        ...
+        if reason is None:
+            reason = 'No Reason'
+        else:
+            pass
+        await self.afk_on(user=ctx.message.author.id, reason=reason)
+        await self.bot.say("<:idle:483080613687984131> I have successfully set you as **AFK**!")
       
     @afk.command(pass_context=True, aliases=['disable'])
     async def off(self, ctx):
         """Turn AFK Off (AFK SUB)"""
-        ...
+        await self.afk_off(user=ctx.message.author.id)
+        await self.bot.say(f"<:online:313956277808005120> Welcome back, **{ctx.message.author}**!")
         
         
     async def afk_on(self, user=None, reason=None):
