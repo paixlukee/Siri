d504ba4ecf008456730ba485b11282cbfe9183b3
iWeeti <40580113+iWeeti@users.noreply.github.com>
2018-09-11 05:08:03 +0300

    Please read

--- launcher.py
+++ launcher.py
@@ -4,8 +4,9 @@ import asyncio
 
 def run_bot():
 	loop = asyncio.get_event_loop() 
-	bot = Siri()
-	bot.run(config.token)
+	bot = Siri()#also my discord account got disabled, for spam!!!!! while i was god damn sleeping
+	bot.run()#not here it is already in siri.py
+	
 	
 if __name__ == '__main__':
 	run_bot()
