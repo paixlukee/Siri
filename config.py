17df1a0c717229a2e7bef77747bcb857bb80003d
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-10 20:24:40 -0400

    uwu

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 token = '',
-prefixes=['hey siri ', 'siri ', 'Siri ', 'Hey Siri ', 'siri, ']
+prefixes=['hey siri ', 'siri ', 'Siri ', 'Hey Siri ', 'siri, '],
+colours=[0x37749c, 0xd84eaf, 0x45b4dl]
 
 #owo
