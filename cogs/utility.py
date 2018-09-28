6d88a809f1cbf12864efb63d5a1e28fd09476081
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-27 20:23:43 -0400

    Fix remind

--- cogs/utility.py
+++ cogs/utility.py
@@ -94,7 +94,7 @@ class Utility:
             
             
     @commands.command(aliases=['remindme', 'reminder'])
-    async def remind(self, ctx, opt, *, time, *, reason):
+    async def remind(self, ctx, opt, time, *, reason):
         t = self.set_timer(option=opt, time=int(time))
         await asyncio.sleep(t)
         await ctx.send(f"{ctx.author.mention}, :alarm_clock: **Ding!** I was supposed to remind you: `{reason}` ({opt}{time} ago!)")
