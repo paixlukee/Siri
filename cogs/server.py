aa653bd6508963e2fad2e19819230ff8a1ae30a5
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-29 16:37:02 -0400

    Create server.py

---
+++ cogs/server.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+
+class Server:
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def
+      
+def setup(bot):
+  bot.add_cog(Server(bot))
