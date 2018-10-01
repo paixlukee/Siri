87cb96478edef2f43eb7a6c52809fedfa8275bff
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-01 15:55:53 -0400

    Fix lyrics

--- cogs/music.py
+++ cogs/music.py
@@ -63,7 +63,7 @@ class Music:
                     lyrics = f"Seems like these lyrics are too long to display! Click [here]({r['links']['genius']}) to get them.\n\n*Powered by SomeRandomAPI*"
                 else:
                     lyrics = f"{r['lyrics']\n\n*Powered by SomeRandomAPI*"
-                embed = discord.Embed(colour=rnd(self.colour), title=r['title']", description=lyrics, url=r['links']['genius'])
+                embed = discord.Embed(colour=rnd(self.colour), title=r['title'], description=lyrics, url=r['links']['genius'])
                 embed.set_footer(name="Genius", icon_url="https://trashbox.ru/files/427612_ad428e/yp31wbgn.png")
                 await ctx.send(embed=embed)
                 
