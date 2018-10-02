aa457e6d385f8f2be3aedc65d59dbcc9c6c33c3a
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-01 20:56:27 -0400

    Fix weather

--- cogs/utility.py
+++ cogs/utility.py
@@ -106,11 +106,11 @@ class Utility:
             await message.channel.send(f"**{message.author.name}**, {response}")
             
         if message.content.lower().startswith('hey siri, whats the weather in ') or message.content.lower().startswith('hey siri, what\'s the weather in ') or message.content.lower().startswith('hey siri, what is the weather in '):
+            r =  requests.get("f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=metric"").json()
             location = message.content.lower().replace("hey siri, whats the weather in ", "").replace("hey siri, what's the weather in ", "").replace("?", "").replace("!", "").replace("hey siri, what is the weather in ", "")
             async with aiohttp.ClientSession(headers={'Accept': 'application/json'}) as session:
                 async with session.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=Imperial") as get:
                     resp = await get.json()
-                    requests.get("f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=metric"").json()
                     if get.status == 404:
                         return await message.channel.send("I couldn't find that place! Did you spell it correctly?")
                     w = resp['main']['temp']
@@ -758,6 +758,7 @@ class Utility:
     @commands.command()
     async def weather(self, ctx, *, cityname):
         """Weather in a specified location"""
+        r =  requests.get("f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=metric"").json()
         location = cityname
         try:
             await self.add_money(user=ctx.message.author.id, count=1)
@@ -766,7 +767,6 @@ class Utility:
         async with aiohttp.ClientSession(headers={'Accept': 'application/json'}) as session:
             async with session.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=Imperial") as get:
                 resp = await get.json()
-                requests.get("f"https://api.openweathermap.org/data/2.5/weather?q={location}&APPID=f8f21ceb5e624851c948c33ffbe43f1d&units=metric"").json()
                 if get.status == 404:
                     return await ctx.send("I couldn't find that place! Did you spell it correctly?")
                 w = resp['main']['temp']
