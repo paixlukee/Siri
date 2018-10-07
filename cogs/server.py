bbda7571137cc715f8d17db8df184d550ec82940
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-07 00:48:49 -0400

    Fix selfrole

--- cogs/server.py
+++ cogs/server.py
@@ -194,20 +194,20 @@ class Server:
     async def selfrole(self, ctx, opt, role: discord.Role):
         if ctx.guild.id == 493325581606453248:
             if opt == 'remove' or opt == 'r':
-                if role.lower() == 'updates':
+                if role == 'updates':
                     if not "UPDATES" in [x.name for x in ctx.author.roles]:
                         await ctx.send("<:redtick:492800273211850767> I can't remove a role that you don't have!")
                     else:
                         await ctx.send(f"<:greentick:492800272834494474> Self-Role, **UPDATES**, has been successfully removed from **{ctx.author}**!")
-                        await ctx.author.remove_roles(role.upper())    
+                        await ctx.author.remove_roles(role)    
                         
                 else:
                     await ctx.send(f"`{role}` isn't a self-role!")
 
             if opt == 'add' or opt == 'a':
-                if role.lower() == 'updates':
+                if role == 'updates':
                     await ctx.send(f"<:greentick:492800272834494474> Self-Role, **UPDATES**, has been successfully given to **{ctx.author}**!")
-                    await ctx.author.add_roles(role.upper())
+                    await ctx.author.add_roles(role)
                 else:
                     await ctx.send(f"`{role}` isn't a self-role!")
         else:
