bdf76753ac782e0df9ad3f98a40d38df9fb16ac7
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-21 15:26:11 -0400

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -130,7 +130,8 @@ class Utility:
                     else:
                         await message.channel.send(f"Brr. Take a jacket!.. up to **{resp['main']['temp_max']}°F**!")
                     await message.channel.send(embed=embed)
-            if message.content == '@Siri 🎃' or message.content == '<@481337766379126784> ':
+            if message.content.endswith('<@481337766379126784>'):
+                print(f"[PING]: I have been pinged in #{message.channel} by {message.author}")
                 embed = discord.Embed(description=":wave: **How can I help you?**\nFor help, do `siri help`. For support, do `siri support`.", colour=0xf0f0ff)
                 await message.channel.send(embed=embed)
             
