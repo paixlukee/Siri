3257752b4077263b76625105717296fb6e7a2c77
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-21 15:36:54 -0400

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -86,6 +86,7 @@ class Utility:
         if message.content.startswith('<@481337766379126784> '):
             fmsg = message.content
             msg = fmsg.replace("<@481337766379126784> ", "")
+            print(f"[PING]: I have been pinged in #{message.channel} by {message.author}. Content: '{msg}'")
             if msg == '':
                 print(f"[PING]: I have been pinged in #{message.channel} by {message.author}")
                 embed = discord.Embed(description=":wave: **How can I help you?**\nFor help, do `siri help`. For support, do `siri support`.", colour=0xf0f0ff)
