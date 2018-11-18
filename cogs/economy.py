8f3b3281f1277b2a3a7b57d5f31a2cd07836416a
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-11-17 21:44:20 -0500

    Fix economy commands

--- cogs/economy.py
+++ cogs/economy.py
@@ -44,87 +44,76 @@ class Economy:
         - White
 
         """
-        with open('assets/economy.json', 'r') as f:
-            users = json.load(f)
-
+        posts = db.posts.find({"user": ctx.author.id)})
         if colour is None:
-            await ctx.send("`Incorrect Usage`\n```siri setcolour <colour-name>```")
+            await ctx.send("<:redtick:492800273211850767> `Incorrect Usage`\n```siri setcolour <colour-name>```")
 
-        elif str(ctx.author.id) in users:
+        elif not posts is None:
             
-            if colour == 'pink' or colour == 'Pink':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xff93f4)
-                await ctx.send("Set.")
-            elif colour == 'blue' or colour == 'Blue':
-                await self.set_col(users, user=str(ctx.author.id), colour=0x0000ff)
-                await ctx.send("Set.")
-            elif colour == 'green' or colour == 'Green':
-                await self.set_col(users, user=str(ctx.author.id), colour=0x00ff00)
-                await ctx.send("Set.")
-            elif colour == 'red' or colour == 'Red':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xff0000)
-                await ctx.send("Set.")
-            elif colour == 'violet' or colour == 'Violet':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xa341f4)
-                await ctx.send("Set.")
-            elif colour == 'orange' or colour == 'Orange':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xff9d00)
-                await ctx.send("Set.")
-            elif colour == 'yellow' or colour == 'Yellow':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xffff00)
-                await ctx.send("Set.")
-            elif colour == 'white' or colour == 'White':
-                await self.set_col(users, user=str(ctx.author.id), colour=0xffffff)
-                await ctx.send("Set.")
+            if colour.lower() == 'pink': # This is a reeaallly bad way of doing it. enh.
+                await self.set_col(user=str(ctx.author.id), colour=0xff93f4)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'blue':
+                await self.set_col(user=str(ctx.author.id), colour=0x0000ff)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'green':
+                await self.set_col(user=str(ctx.author.id), colour=0x00ff00)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'red':
+                await self.set_col(user=str(ctx.author.id), colour=0xff0000)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'violet' or colour.lower() == 'purple':
+                await self.set_col(user=str(ctx.author.id), colour=0xa341f4)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'orange':
+                await self.set_col(user=str(ctx.author.id), colour=0xff9d00)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'yellow':
+                await self.set_col(user=str(ctx.author.id), colour=0xffff00)
+                await ctx.send("<:greentick:492800272834494474> Set.")
+            elif colour.lower() == 'white':
+                await self.set_col(user=str(ctx.author.id), colour=0xffffff)
+                await ctx.send("<:greentick:492800272834494474> Set.")
             else:
-                await ctx.send("That isn't a valid colour! View the colour list with `siri help cmd setcolour`\n```If you would like to suggest a colour, send a ticket ('siri ticket <message>') with the colour name and hex.```")
-            
-            with open('assets/economy.json', 'w') as f:
-                json.dump(users, f)
+                await ctx.send("<:redtick:492800273211850767> That isn't a valid colour! View the colour list with `siri help cmd setcolour`\n```If you would like to suggest a colour, send a ticket ('siri ticket <message>') with the colour name and hex.```")
         else:
-            await ctx.send("You don't have a bank account, create one with `siri bank create`!")
+            await ctx.send("<:redtick:492800273211850767> You don't have a bank account, create one with `siri bank create`!")
 
     @commands.command(pass_context=True)
     async def description(self, ctx, *, message):
         """Sets Profile Description"""
-        with open(r'assets\economy.json', 'r') as f:
-            users = json.load(f)
+        posts = db.posts.find({"user": ctx.author.id)})
 
         if message is None:
-            await ctx.send("`Incorrect Usage`\n```siri description <description>```")
+            await ctx.send("<:redtick:492800273211850767> `Incorrect Usage`\n```siri description <description>```")
 
         elif len(message) > 400:
-            await ctx.send("Description cannot be longer than 400 characters!")
+            await ctx.send("<:redtick:492800273211850767> Description cannot be longer than 400 characters!")
 
-        elif str(ctx.author.id) in users:
-            await ctx.send("Set.")
-            await self.set_desc(users, user=str(ctx.author.id), description=message)
-            with open('assets\\economy.json', 'w') as f:
-                json.dump(users, f)
+        elif not posts is None:
+            await ctx.send("<:greentick:492800272834494474> Set.")
+            await self.set_desc(user=ctx.author.id, description=message)
         else:
-            await ctx.send("You don't have a bank account, create one with `siri bank create`!")
+            await ctx.send("<:redtick:492800273211850767> You don't have a bank account, create one with `siri bank create`!")
 
     @commands.command(aliases=['birthday'])
     async def bday(self, ctx, day=None, month=None, year=None):
         """Sets Profile Birthday"""
-        with open('assets\\economy.json', 'r') as f:
-            users = json.load(f)
+        posts = db.posts.find({"user": ctx.author.id)})
         try:
             date = day.split('-')
         except:
             pass
         if day is None:
-            await ctx.send("`Incorrect Usage`\n```siri birthday DD-MM-YYYY```") 
+            await ctx.send("<:redtick:492800273211850767> `Incorrect Usage`\n```siri birthday DD-MM-YYYY```") 
         elif len(date) < 3:
-            await ctx.send("`Incorrect Format`\n```DD-MM-YYYY```")           
-        elif str(ctx.author.id) in users:
-            await ctx.send("Set.")
+            await ctx.send("<:redtick:492800273211850767> `Incorrect Format`\n```DD-MM-YYYY```")           
+        elif not posts is None:
+            await ctx.send("<:greentick:492800272834494474> Set.")
             bday = date
-            await self.set_bday(users, user=str(ctx.author.id), date=bday)
-            with open('assets\\economy.json', 'w') as f:
-                json.dump(users, f)
+            await self.set_bday(user=ctx.author.id, date=bday)
         else:
-            await ctx.send("You don't have a bank account, create one with `siri bank create`!")
+            await ctx.send("<:redtick:492800273211850767> You don't have a bank account, create one with `siri bank create`!")
 
     @commands.command(aliases=['market', 'shop'])
     async def store(self, ctx):
@@ -134,70 +123,68 @@ class Economy:
         embed.add_field(name=f"[2] 📱 iPhone ({self.s}300)", value=f"Is that an iPhone 4? The best phone ever tbh")
         embed.add_field(name=f"[3] 🏠 House ({self.s}2000)", value=f"Wait.. since when can you buy homes at the Apple Store?")
         embed.set_footer(text="To buy an item, do 'siri buy <item>' or 'siri buy <item-number>'")
-        # embed.set_thumbnail(url="https:/c.slashgear.com/wp-content/uploads/2016/06/apple-store-800x420.jpg")
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
     async def eat(self, ctx):
         """Eat an apple"""
-        with open('assets/economy.json', 'r') as f:
-            users = json.load(f)
-        if str(ctx.author.id) in users:
-            if users[str(ctx.author.id)]['apple'] > 0:
-                responses = ['Yum.', 'Mmm.', 'Tasty?', 'Mm. Sounds good!']
+        posts = db.posts.find({"user": ctx.author.id)})
+        if not posts is None:
+            if posts['apple'] > 0:
+                responses = ['Yum.', 'Mmm.', 'Tasty?', 'Were you hungry?', 'How is it?']
                 await ctx.send(f"You ate :apple:`1`! | {random.choice(responses)}") 
                 await self.apple(users, user=str(ctx.author.id), count=-1)
             else:
-                await ctx.send("You don't have any apples!")
+                await ctx.send("<:redtick:492800273211850767> You don't have any apples!")
         else:
-            await ctx.send("You don't have a bank account, create one with `siri bank create`!")
+            await ctx.send("<:redtick:492800273211850767> You don't have a bank account, create one with `siri bank create`!")
         
 
     @commands.command(pass_context=True)
     async def buy(self, ctx, item = None):
         """Buy an item ('siri store' to see the items)"""
-        posts = db.posts.find({"user": member.id)})
+        posts = db.posts.find({"user": ctx.author.id)})
         msg = await ctx.send("Processing..")
         if posts is None:
-            await ctx.send("You don't have a bank account, create one with `siri bank create`!")
+            await ctx.send("<:redtick:492800273211850767> You don't have a bank account, create one with `siri bank create`!")
         elif item is None:
-            await ctx.send("`Incorrect Usage`\n```siri buy <item>```")
+            await ctx.send("<:redtick:492800273211850767> `Incorrect Usage`\n```siri buy <item>```")
             await msg.delete()
         elif item.lower() == 'apple' or item == '1':
-            if users[str(ctx.author.id)]['money'] < 2:
-                await ctx.send("You don't have enough to buy this item!")
+            if posts['money'] < 2:
+                await ctx.send("<:redtick:492800273211850767> You don't have enough to buy this item!")
                 await msg.delete()
             else:
-                await self.take_money(user=str(ctx.author.id), count=2)
-                await self.apple(user=str(ctx.author.id), count=1)
+                await self.take_money(user=ctx.author.id, count=2)
+                await self.apple(user=ctx.author.id, count=1)
                 await msg.delete()
-                await ctx.send("You have successfully bought :apple:`1` from the Apple Store!")
+                await ctx.send("<:greentick:492800272834494474> You have successfully bought :apple:`1` from the Apple Store!")
         elif item.lower() == 'iphone' or item == '2':
             chance = random.randint(1, 25)
             chance_b = random.randint(1, 25)
             if posts['money'] < 300:
-                await ctx.send("You don't have enough to buy this item!")
+                await ctx.send("<:redtick:492800273211850767> You don't have enough to buy this item!")
                 await msg.delete()
             elif chance == chance_b:
-                await self.take_money(user=str(ctx.author.id), count=300)
+                await self.take_money(user=ctx.author.id, count=300)
                 await msg.delete()
                 await ctx.send(f"**BOGO!** You have won :iphone:`2` for the price of one!")
-                await self.iphone(user=str(ctx.author.id), count=2)
+                await self.iphone(user=ctx.author.id, count=2)
             else:
-                await self.take_money(user=str(ctx.author.id), count=300)
+                await self.take_money(user=ctx.author.id, count=300)
                 await msg.delete()
-                await ctx.send("You have successfully bought :iphone:`1` from the Apple Store!")
-                await self.iphone(user=str(ctx.author.id), count=1)
-        elif item == 'house' or item == 'House' or item == '3': #2,000
+                await ctx.send("<:greentick:492800272834494474> You have successfully bought :iphone:`1` from the Apple Store!")
+                await self.iphone(user=ctx.author.id, count=1)
+        elif item.lower() == 'house' or item == '3':
             await msg.delete()
-            if users[str(ctx.author.id)]['money'] < 2000:
-                await ctx.send("You don't have enough to buy this item!")
+            if posts['money'] < 2000:
+                await ctx.send("<:redtick:492800273211850767> You don't have enough to buy this item!")
             else:
-                await self.take_money(users, user=str(ctx.author.id), count=2000)
-                await ctx.send("You have successfully bought :house:`1` from the Apple Store!")
-                await self.house(users, user=str(ctx.author.id), count=1)
+                await self.take_money(user=ctx.author.id, count=2000)
+                await ctx.send("<:greentick:492800272834494474> You have successfully bought :house:`1` from the Apple Store!")
+                await self.house(user=ctx.author.id, count=1)
         else:
-            await ctx.send("I couldn't find that item.. Do `siri shop` to see what we have..")
+            await ctx.send("<:redtick:492800273211850767> I couldn't find that item.. Do `siri shop` to see what we have..")
 
     @commands.command(aliases=['Profile'])
     async def profile(self, ctx, user: discord.User=None):
@@ -339,40 +326,40 @@ class Economy:
         }
         db.posts.insert_one(post)
 
-    async def apple(self, user=None, count=None):
+    async def apple(self, user:int, count=None):
         data = db.posts.find_one({"user": user})
         bal = data['apple']
         count = bal + count
         db.posts.update_one({"user": user}, {"$set":{"apple": count}})
 
-    async def iphone(self, user=None, count=None):
+    async def iphone(self, user:int, count=None):
         data = db.posts.find_one({"user": user})
         bal = data['iphone']
         count = bal + count
         db.posts.update_one({"user": user}, {"$set":{"iphone": count}})
 
-    async def house(self, user=None, count=None):
+    async def house(self, user:int, count=None):
         data = db.posts.find_one({"user": user})
         bal = data['house']
         count = bal + count
         db.posts.update_one({"user": user}, {"$set":{"house": count}})
 
-    async def set_desc(self, user=None, description=None):
+    async def set_desc(self, user:int, description=None):
         db.posts.update_one({"user": user}, {"$set":{"birthday": date}})
 
-    async def set_bday(self, user=None, date=None):
+    async def set_bday(self, user:int, date=None):
         db.posts.update_one({"user": user}, {"$set":{"birthday": date}})
 
-    async def set_col(self, user=None, colour=None):
+    async def set_col(self, user:int, colour=None):
         db.posts.update_one({"user": user}, {"$set":{"colour": colour}})
 
-    async def add_money(self, user, count):
+    async def add_money(self, user:int, count):
         data = db.posts.find_one({"user": user})
         bal = data['money']
         money = bal + count
         db.posts.update_one({"user": user}, {"$set":{"money": money}})
 
-    async def take_money(self, user, count):
+    async def take_money(self, user:int, count):
         data = db.posts.find_one({"user": user})
         bal = data['money']
         money = bal - count
