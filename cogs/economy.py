157f520b0ddf19006fc4aeeea1deec657ac00455
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-11-22 23:00:15 -0500

    ok

--- cogs/economy.py
+++ cogs/economy.py
@@ -35,10 +35,10 @@ class Economy:
         #pages = math.ceil(len([x for x in db.posts.find()]) / 12)
         clb = ''
 
-        for i, x in [x for x in db.posts.find()]:
-            #name = self.bot.get_user(x['user'])
-            #money = x['money']
-            clb += 'pp'#f'**{i + 1}.** **{name}** - **{self.s}{money}**'
+        for i, x in [x for x in db.posts.find({"user": ctx.author.id})]:
+            name = self.bot.get_user(x['user'])
+            money = x['money']
+            clb += f'**{i + 1}.** **{name}** - **{self.s}{money}**'
             
         embed = discord.Embed(colour=0x37749c, description=clb)
         embed.set_author(name="Leaderboard", icon_url=ctx.me.avatar_url_as(format='png'))
