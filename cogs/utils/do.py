be9a83df42a329b4f002633382566ff41e89733e
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-13 21:48:58 -0500

    Fix post()

--- cogs/utils/do.py
+++ cogs/utils/do.py
@@ -9,14 +9,14 @@ import config
 
 dbl_token = config.dbl_token
 
-async def post(list:str):
+async def post(count, list:str):
   lists = ['dbl']
   if not list.lower() in lists:
       return f"'{list}' list not available."
   else:
       if list.lower() == 'dbl':
           try:
-              data = {'server_count': len(self.bot.guilds)}
+              data = {'server_count': count}
               headers = {'Authorization': dbl_token}
               p = requests.post('https://discordbots.org/api/bots/481337766379126784/stats', data=data, headers=headers).json()
               return "Server count has been posted.`"
