d69c488780c3dec8b2520c3cbda25c8e8006aace
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-01-05 14:19:03 -0500

    Add check() and all()

---
+++ cogs/utils/patron.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+import pymongo
+
+client = MongoClient(config.mongo_client)
+db = client['siri']
+
+async def check(id):
+    patron = db.utility.find_one({"utility": "patrons"})
+    if id in patron['gold']:
+        return 'GOLD'
+    elif id in patron['silver']:
+        return 'SILVER'
+    elif id in patron['bronze']:
+        return 'BRONZE'
+    else:
+        return None
+
+async def all():
+    patron = db.utility.find_one({"utility": "patrons"})
+    all_list = []
+    for x in patron['gold']:
+        all_list.append(x)
+    for x in patron['silver']:
+        all_list.append(x)
+    for x in patron['bronze']:
+        all_list.append(x)
+    return all_list
