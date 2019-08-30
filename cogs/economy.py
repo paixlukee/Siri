e19cf9cc1998d0b0e609579b65e3b59e258d12f7
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-08-30 16:38:07 -0400

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -293,7 +293,6 @@ class Economy:
             await ctx.send('c')
         else:
             resp = random.choice(posts['work'])
-            print(posts['work'])
             embed = discord.Embed(description=f"{resp['text']} You have been paid {self.s}{resp['money']}.")
             embed.set_footer(text=f"Work again in 24 Hours!")
             await ctx.send(embed=embed)
