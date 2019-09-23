df4811b10c2193b742823c8ce2b368820016ee05
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-09-23 18:45:31 -0400

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -86,14 +86,14 @@ class Levels:
             await ctx.send(f"{user.name}'s siri level is {data['level']} (cmd is WIP)")
         else:
             data = db.posts.find_one({"user": ctx.author.id})
-            img = Image.open(f"sirirankcard.jpg")
+            img = Image.open(f"\\sirirankcard.jpg")
             draw = ImageDraw.Draw(img)
             font = ImageFont.truetype('impact.ttf', 40)
             draw.text((49,61), ctx.author, font=font, fill=(255, 255, 255, 255))
             bytes = BytesIO()
             img.save(bytes, 'PNG')
             bytes.seek(0)
-            await ctx.send(file="sirirankcard.jpg")
+            await ctx.send(file="\\sirirankcard.jpg")
             #await ctx.send(f"{ctx.author.name}\'s siri level is {data['level']} (cmd is WIP)")
         
     @commands.command()
