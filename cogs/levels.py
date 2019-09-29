996496923fb039a80f0e0ffb4a1cd6c00d11911f
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-09-28 23:55:19 -0400

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -90,8 +90,8 @@ class Levels:
             card_link = Image.open("sirirankcard.jpg")
             #img = Image.open(BytesIO(card_link.content))
             draw = ImageDraw.Draw(card_link)
-            font_size = 30
-            if len(str(ctx.author)) > 20:
+            font_size = 14
+            if len(str(ctx.author)) > 26:
                 font_size = 14
             elif len(str(ctx.author)) > 15:
                 font_size = 17
