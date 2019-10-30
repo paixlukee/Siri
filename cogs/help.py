b442578b176174be7452c1254e45cccc2dc552f9
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-10-30 18:24:30 -0400

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -14,10 +14,10 @@ class Help:
     def __init__(self, bot):
         self.bot = bot
         self.colours = [0x37749c, 0xd84eaf, 0x45b4de, 0x42f4c5, 0xffb5f3, 0x42eef4, 0xe751ff, 0x51ffad]
-        self.utility = ["`article`", "`avatar`", "`chatbot`", "`colour`", "`define`","`discordstatus`", "`hastebin`","`IMDb`","`langdetect`", "`map`", "`news`","`shorten`", "`search`","`serverinfo`", "`userinfo`","`strawpoll`", "`timer`","`translate`", "`weather`", "`wikipedia`"]
+        self.utility = ["article", "avatar", "chatbot", "colour", "define","discordstatus", "hastebin","IMDb","langdetect", "map", "news","shorten", "search","serverinfo", "userinfo","strawpoll", "timer","translate", "weather`", "wikipedia"]
         self.botc = ["`ticket`", "`stats`", "`support`", "`ping`"]
-        self.economy = ["`balance`", "`bank create`", "`birthday`", "`buy`","`daily`","`description`", "`eat`", "`flip`","`give`", "`level`","`lvlmsgs`","`leaderboard`", "`profile`","`setcolour`", "`shop`", "`slots`"]
-        self.music = ["`play`", "`queue`", "`np`", "`shuffle`", "`repeat`","`stop`", "`volume`", "`pause`", "`resume`", "`leave`", "`msearch`", "`remove`"]
+        self.economy = ["balance", "bank create", "birthday", "buy","daily","description", "eat", "flip","give", "level","lvlmsgs","leaderboard", "profile","setcolour", "shop", "slots"]
+        self.music = ["play", "queue", "np", "shuffle", "repeat","stop", "volume", "pause", "resume", "leave", "msearch", "remove"]
         self.news = db.utility.find_one({"utility": "help"})
         
     @commands.command(aliases=['cmds', 'Help'])
@@ -25,10 +25,10 @@ class Help:
     async def help(self, ctx, cmd:str=None):
         if not cmd:
             embed = discord.Embed(colour=rnd(self.colours), title="What can I help you with?", description="For help with a command, do `siri help command`.")
-            embed.add_field(name="Bot", value=" ".join(self.botc))
-            embed.add_field(name="Utility", value=" ".join(self.utility))
-            embed.add_field(name="Economy", value=" ".join(self.economy))
-            embed.add_field(name="Music", value=" ".join(self.music))
+            embed.add_field(name="Bot", value=" | ".join(self.botc))
+            embed.add_field(name="Utility", value=" | ".join(self.utility))
+            embed.add_field(name="Economy", value=" | ".join(self.economy))
+            embed.add_field(name="Music", value=" | ".join(self.music))
             embed.set_footer(text="Help Menu", icon_url="https://cdn.discordapp.com/icons/493325581606453248/5b26d49a78c617fbba0e9cf17c5d8ff0.png?size=1024")
             embed.set_image(url="http://media.idownloadblog.com/wp-content/uploads/2016/06/iOS-10-Siri-waveform-image-001.png")
             await ctx.send(embed=embed)
@@ -42,7 +42,7 @@ class Help:
                 else:
                     _help = "> <".join(_cmd.clean_params)
                     desc = _cmd.help
-                    if not _cmd.clean_params:
+                    if _cmd.clean_params:
                         params = f"<{_help}>"
                     else:
                         params = _help
