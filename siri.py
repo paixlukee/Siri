a6ce1e584be8a32c2b0ebf3104545e2cf1743148
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-10-30 23:13:07 -0400

    Transfer commands to cogs.bot

--- siri.py
+++ siri.py
@@ -25,14 +25,7 @@ class Siri(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(command_prefix=config.prefixes)
         self.remove_command("help")
-        self.add_command(self.shutdown)
-        self.add_command(self.restart)
-        self.add_command(self.pull)
-        self.add_command(self.shell)
-        self.add_command(self.ping)
-        self.add_command(self.load)
-        self.add_command(self.unload)
-        self.add_command(self.reload)
+
         
     async def on_message_edit(self, before, after):
         if not self.is_ready() or after.author.bot:
@@ -66,6 +59,7 @@ class Siri(commands.AutoShardedBot):
         print(f"[AWAITING] Run 'siri load all'")
         self.loop.create_task(self.status_task())
         embed = discord.Embed(title='⚡ **Siri** is connected!', description=f"**Guilds**.. `{str(len(self.guilds))}`")
+        await self.load_extension("cogs.bot")
         try:
             await log.send(embed=embed)
         except:
@@ -105,113 +99,6 @@ class Siri(commands.AutoShardedBot):
         embed.set_footer(text=f'ID: {guild.id}', icon_url=guild.icon_url_as(format='png'))
         await log.send(embed=embed)
 
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def shutdown(self, ctx):
-        await ctx.send("Goodbye. Nice talking to you.")
-        await self.logout()
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def restart(self, ctx):
-        await ctx.send("I'll see you in a bit!")
-        await self.logout()
-        subprocess.call([sys.executable, r"launcher.py"])
-            
-            
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def pull(self, ctx):
-        msg = await ctx.send("Pulling..")
-        shell = await self.run_cmd('git pull Siri --no-commit --no-edit --ff-only master')
-        shell = str(shell)
-        shell = shell.replace("https://github.com/paixlukee/Siri", "Github")
-        embed = discord.Embed(colour=0x0000ff, description=f"```css\n{shell}```")
-        embed.set_author(name="Pulled from Git..", icon_url="https://avatars0.githubusercontent.com/u/9919?s=280&v=4")
-        await msg.delete()
-        await ctx.send(embed=embed)
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def shell(self, ctx, *, code):
-        embed = discord.Embed(colour=0x000fff, description=f"```css\nConnecting to shell..```")
-        embed.set_author(name="Please Wait.", icon_url=self.user.avatar_url)
-        msg = await ctx.send(embed=embed)
-        shell = await self.run_cmd(code)
-        embed = discord.Embed(colour=0x000fff, description=f"```css\n{shell}```")
-        embed.set_author(name="Shell", icon_url=self.user.avatar_url)
-        await msg.delete()
-        await ctx.send(embed=embed)
-
-
-    @commands.command(pass_context=True)
-    async def ping(self, ctx):
-        """Get Siri's Ping"""
-        t1 = time.perf_counter()
-        await ctx.trigger_typing()
-        t2 = time.perf_counter()
-        ping_desc = ("Ping: `" + str(round((t2-t1)*1000)) + "ms`!")
-        embed = discord.Embed(description=ping_desc)
-        await ctx.send(embed=embed)
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def load(self, ctx, extension):
-        try:
-            if extension == 'all':
-                print(f'[UPDATE] Loaded all modules')
-                print("------\n\n")
-                loaded = []
-                not_loaded = []
-                for extension in extensions:
-                    try:
-                        self.load_extension(extension)
-                        loaded.append(f'`{extension}`')    
-                    except Exception as error:
-                        not_loaded.append(f'`{extension}` - `{error}`')
-                        print('\n\nEXTEN./COG ERROR: {} was not loaded due to an error: \n-- [{}] --\n\n'.format(extension, error))
-                    
-                loaded = '\n'.join(loaded)
-                not_loaded = '\n'.join(not_loaded)
-                embed = discord.Embed(colour=0x0000ff)
-                embed.add_field(name='Loaded', value=loaded)
-                if not_loaded is None:
-                    embed.add_field(name='Not Loaded', value=not_loaded)
-                        
-                await ctx.send(embed=embed)
-            else:
-                self.load_extension("cogs.{}".format(extension))
-                embed = discord.Embed(title="<:CheckMark:473276943341453312> Cog loaded:", color=0x5bff69, description="**Cog:** `cogs\{}.py`".format(extension))
-                await ctx.send(embed=embed)
-                print('\n\nCOG LOAD\n--[Cog loaded, {}.py]--\n\n'.format(extension))
-        except Exception as error:
-            print('\n\nEXTEN./COG ERROR: {} was not loaded due to an error: \n-- [{}] --\n\n'.format(extension, error))
-            embed = discord.Embed(title="<:WrongMark:473277055107334144> Error loading cog:", color=0xff775b, description="**Cog:** `cogs\{}.py`\n**Errors:**\n```{}```".format(extension, error))
-            await ctx.send(embed=embed)
-
-    @commands.command(aliases=['un'], hidden=True)
-    @commands.is_owner()
-    async def unload(self, ctx, extension):
-        self.unload_extension("cogs.{}".format(extension))
-        embed = discord.Embed(title="<:CheckMark:473276943341453312> Cog unloaded:", color=0x5bff69, description="**Cog:** `cogs\{}.py`".format(extension))
-        await ctx.send(embed=embed)
-            
-
-    @commands.command(aliases=['re'], hidden=True)
-    @commands.is_owner()
-    async def reload(self, ctx, extension):
-        try:
-            self.unload_extension("cogs.{}".format(extension))
-            self.load_extension("cogs.{}".format(extension))
-            embed = discord.Embed(title="<:CheckMark:473276943341453312> Cog reloaded:", color=0x5bff69, description="**Cog:** `cogs\{}.py`".format(extension))
-            await ctx.send(embed=embed)
-            print('\n\nCOG RELOAD\n--[Cog reloaded, {}.py]--\n\n'.format(extension))
-        except Exception as error:
-            print('\n\nEXTEN./COG ERROR: {} was not reloaded due to an error: \n-- [{}] --\n\n'.format(extension, error))
-            embed = discord.Embed(title="<:WrongMark:473277055107334144> Error reloading cog:", color=0xff775b, description="**Cog:** `cogs\{}.py`\n**Errors:**\n```{}```".format(extension, error))
-            await ctx.send(embed=embed)
-
-    #on
 
     async def on_message(self, message):
         if message.author.bot: return
